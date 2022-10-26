bf5e14b011e919f4124d0f5c906df9814b24e7d9
Tiago Cardoso <tiagocardoso.dev@gmail.com>
2022-10-26 15:06:23 -0300

    Refinamento do relacionamento entre as entidades

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -5,4 +5,10 @@ public class Categoria
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
     public string? ImagemUrl { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }
+
+    public Categoria()
+    {
+        Produtos = new List<Produto>();
+    }
 }
