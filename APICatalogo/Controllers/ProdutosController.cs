9a8bedc6ceda3e88c892228a2e00236afd8d9792
Tiago Cardoso <tiagocardoso.dev@gmail.com>
2022-10-27 17:58:53 -0300

    Add Nullable notation

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -18,7 +18,7 @@ public class ProdutosController : ControllerBase
     [HttpGet]
     public ActionResult<IEnumerable<Produto>> Get()
     {
-        var produtos = _context.Produtos.ToList();
+        var produtos = _context.Produtos?.ToList();
         if(produtos is null)
         {
             return NotFound("¯¯\\_(ヅ)_/¯¯ Poxa, não encontramos nenhum produto em nossa base.");
@@ -29,7 +29,7 @@ public class ProdutosController : ControllerBase
     [HttpGet("{id:int}")]
     public ActionResult<Produto> Get(int id)
     {
-        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+        var produto = _context.Produtos?.FirstOrDefault(p => p.ProdutoId == id);
         if(produto is null)
         {
             return NotFound("¯¯\\_(ヅ)_/¯¯ Poxa, seu produto não foi encontrado.");
